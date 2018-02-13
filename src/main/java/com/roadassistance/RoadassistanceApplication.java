2bc9bf0508059d9b7faf631f240343ef1c8f7510
1 <1@DmitriyK>
2018-02-13 13:44:49 +0200

    First commit

---
+++ src/main/java/com/roadassistance/RoadassistanceApplication.java
@@ -0,0 +1,12 @@
+package com.roadassistance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadassistanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadassistanceApplication.class, args);
+	}
+}
---
+++ src/test/java/com/roadassistance/RoadassistanceApplicationTests.java
@@ -0,0 +1,16 @@
+package com.roadassistance;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class RoadassistanceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
